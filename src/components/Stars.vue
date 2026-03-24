aa8ad0e6b2be941106b325f329208dad23867a1a
Kenzo Ruthes dos Santos <kenzo.santos@alunos.sc.senac.br>
2026-03-24 15:33:23 -0300

    criar composable useTarefas com computed e watch

---
+++ src/components/Stars.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>Componente Stars</h1>
+  <ion-but  ton
+    v-for="(value, indice) in 5"
+    :class="amarelo: indice < total"
+    :key="indice"
+  ></ion-button>
+</template>
+
+<script setup>
+import { defineProps } from "vue";
+
+import { IonButton } from "@ionic/vue";
+
+const props = defineProps({
+  total: { type: Number, default: 0 },
+});
+</script>
