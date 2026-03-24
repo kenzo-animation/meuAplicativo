993cc9751b2baa5b14fe4d5195517ea749d7bf30
Kenzo Ruthes dos Santos <kenzo.santos@alunos.sc.senac.br>
2026-03-24 15:35:33 -0300

    criar CardTarefa com props e emits

---
+++ src/components/CardTarefa.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+interface Tarefa {
+  id: number;
+  texto: string;
+  feita: boolean;
+}
+const props = defineProps<{ tarefa: Tarefa }>();
+const emit = defineEmits<{
+  remover: [id: number];
+  concluir: [id: number];
+}>();
+</script>
+<template>
+  <IonCard>
+    <IonCardContent>
+      <IonItem lines="none">
+        <IonIcon
+          slot="start"
+          :icon="props.tarefa.feita ? checkmarkCircle : ellipseOutline"
+          :color="props.tarefa.feita ? 'success' : 'medium'"
+          @click="emit('concluir', props.tarefa.id)"
+        />
+        <IonLabel
+          :style="props.tarefa.feita ? 'text-decoration:linethrough' : ''"
+        >
+          {{ props.tarefa.texto }}
+        </IonLabel>
+        <IonButton
+          slot="end"
+          fill="clear"
+          color="danger"
+          @click="emit('remover', props.tarefa.id)"
+        >
+          <IonIcon :icon="trashOutline" />
+        </IonButton>
+      </IonItem>
+    </IonCardContent>
+  </IonCard>
+</template>
