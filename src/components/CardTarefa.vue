0fd96e69995bec36328ee98f183a0190711569b0
Kenzo Ruthes dos Santos <kenzo.santos@alunos.sc.senac.br>
2026-03-24 16:07:55 -0300

    botoens de excluir e finalizar adicionados

--- src/components/CardTarefa.vue
+++ src/components/CardTarefa.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { IonCard, IonCardContent, IonItem, IonIcon, IonLabel, IonButton } from "@ionic/vue";
+import { checkmarkCircle, ellipseOutline, trashOutline } from "ionicons/icons";
 
 interface Tarefa {
   id: number;
@@ -18,23 +19,31 @@ const emit = defineEmits<{
       <IonItem lines="none">
         <IonIcon
           slot="start"
-          :icon="props.tarefa.feita ? 'checkmark-circle' : 'ellipse-outline'"
+          :icon="props.tarefa.feita ? checkmarkCircle : ellipseOutline"
           :color="props.tarefa.feita ? 'success' : 'medium'"
           @click="emit('concluir', props.tarefa.id)"
         />
         <IonLabel
-          :style="props.tarefa.feita ? 'text-decoration:linethrough' : ''"
+          :style="props.tarefa.feita ? 'text-decoration:line-through' : ''"
         >
           {{ props.tarefa.texto }}
         </IonLabel>
-        <IonButton
-          slot="end"
-          fill="clear"
-          color="danger"
-          @click="emit('remover', props.tarefa.id)"
-        >
-          <IonIcon :icon="'trash-outline'" />
-        </IonButton>
+        <div class="ion-padding-start">
+          <IonButton
+            fill="outline"
+            color="primary"
+            @click="emit('concluir', props.tarefa.id)"
+          >
+            {{ props.tarefa.feita ? 'Reabrir' : 'Finalizar' }}
+          </IonButton>
+          <IonButton
+            fill="clear"
+            color="danger"
+            @click="emit('remover', props.tarefa.id)"
+          >
+            <IonIcon :icon="trashOutline" /> Excluir
+          </IonButton>
+        </div>
       </IonItem>
     </IonCardContent>
   </IonCard>
